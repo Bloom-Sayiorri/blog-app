e0e820d3bacdd68baa5f37022a1ab52deffbed92
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-07-22 12:31:16 +0300

    initial commit

---
+++ types/types.ts
@@ -0,0 +1,14 @@
+export type User = {
+    username: string;
+    eamil: string;
+    password: string;
+    age: number;
+    avatar: string;
+    bio: string;
+}
+
+export type Blog = {
+    title: string;
+    category: string;
+    content: string;
+}
